937c2046b41065c7fa01c308da40f58fa59c6c3d
abhishek913 <74429793+abhishek913@users.noreply.github.com>
2023-11-18 12:04:36 -0500

    indent

--- lib/main.dart
+++ lib/main.dart
@@ -60,10 +60,8 @@ class _MyHomePageState extends State<MyHomePage> {
     switch(selectedIndex){
       case 0:
       page = GeneratorPage();
-      break;
       case 1:
       page = FavaoritesPage();
-      break;
       default:
 throw UnimplementedError("This page is not implemneted");
     }
